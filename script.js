4fb1c23283b0d5ac1d72b26ed4c89074d4935e2e
Dante Tokay <valley4stanlley@gmail.com>
2020-04-04 17:13:39 +0200

    condition if is to change

---
+++ script.js
@@ -0,0 +1,16 @@
+const buttonAdd = document.querySelector('.baton');
+  let licznik = 1;
+const addElement = function (){
+
+  const ddiv = document.createElement('DIV');
+  ddiv.textContent=licznik;
+  if(licznik==4){
+    ddiv.classList.add('circle')
+  }
+
+  document.body.appendChild(ddiv);
+  licznik ++;
+
+}
+
+buttonAdd.addEventListener('click', addElement);
